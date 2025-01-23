04673cd94a62beb284f190fdc4b8ffdeebd24188
axendround <94460986+axendround@users.noreply.github.com>
2025-01-24 01:07:55 +0700

    a

--- scripts.js
+++ scripts.js
@@ -14,14 +14,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
         if (languageButton.innerText === "Switch to Vietnamese") {
             bio.innerHTML = `
-                Chào, tớ là Axendround, một thực thể điện toán đến từ <span class="highlight">Việt Nam</span>. Tớ đam mê công nghệ.<br>
+                Chào, tớ là Axendround, một thực thể điện toán đến từ <span class="highlight">Việt Nam</span>.<br>
                 Tớ học ngành <span class="highlight">Mạng máy tính và Truyền thông dữ liệu</span> tại trường <a href="https://utt.edu.vn/" class="link" target="_blank">Đại học Công nghệ Giao thông Vận tải</a>.<br>
                 Tớ có đam mê về hệ thống, đặc biệt là lưu trữ dữ liệu. Hiện tại đang hướng tới lĩnh vực <span class="highlight">Quản trị Hệ thống</span>.<br>
             `;
             languageButton.innerText = "Switch to English";
         } else {
             bio.innerHTML = `
-                Hi, I am Axendround, a cybernetical being based in <span class="highlight">Vietnam</span>. I'm obsessed with tech.<br>
+                Hi, I am Axendround, a cybernetical being based in <span class="highlight">Vietnam</span>.<br>
                 I major in <span class="highlight">Computer Networks and Data Communications</span> at the <a href="https://utt.edu.vn/" class="link" target="_blank">University of Transport Technology</a>.<br>
                 I'm passionate about working with systems, particularly data storage. Currently focusing on <span class="highlight">System Administration</span>.<br>
             `;
